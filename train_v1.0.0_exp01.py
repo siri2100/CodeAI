cb83e6c9e0717cc37c38fe3efb46fa8ad8e79608
jjkim95_desktop <a01095658003@gmail.com>
2023-06-11 19:07:18 +0900

    v1.0.0 exp01

--- train_v1.0.0_exp01.py
+++ train_v1.0.0_exp01.py
@@ -1,4 +1,3 @@
-import torch.nn as nn
 from datasets import load_dataset
 from transformers import AutoTokenizer
 from transformers import AutoModelForSeq2SeqLM
@@ -6,7 +5,6 @@ from transformers import DataCollatorForSeq2Seq
 from transformers import Seq2SeqTrainer
 from transformers import Seq2SeqTrainingArguments
 
-from src.data import map_to_encoder_decoder_inputs
 from src.evaluator import CodeGenerationEvaluator
 
 
@@ -17,54 +15,53 @@ MAX_ENCODER = 32
 MAX_DECODER = 32
 
 
-class SingleGPU(nn.Module)
-    def __init__(self):
-        super(SingleGPU).__init__()
-        tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-nl", use_fast=True)
-        model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-nl")
+def map_to_encoder_decoder_inputs(batch):    
+    inputs = tokenizer(batch["intent"], padding="max_length", truncation=True, max_length=MAX_ENCODER)
+    outputs = tokenizer(batch["snippet"], padding="max_length", truncation=True, max_length=MAX_DECODER)
+    batch["input_ids"] = inputs.input_ids
+    batch["attention_mask"] = inputs.attention_mask
+    batch["decoder_input_ids"] = outputs.input_ids
+    batch["labels"] = outputs.input_ids.copy()
+    batch["decoder_attention_mask"] = outputs.attention_mask
+    return batch
 
-        data = load_dataset("AhmedSSoliman/CoNaLa-Large")
-        trainset = data["train"].map(map_to_encoder_decoder_inputs, tokenizer=tokenizer, batch=BATCH_SIZE, max_encoder=MAX_ENCODER, max_decoder=MAX_DECODER)
-        validset = data["validation"].map(map_to_encoder_decoder_inputs, tokenizer=tokenizer, batch=1, max_encoder=MAX_ENCODER, max_decoder=MAX_DECODER)
-        trainset.set_format(type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"])
-        validset.set_format(type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"])
-        data_collator = DataCollatorForSeq2Seq(
-            tokenizer=tokenizer,
-            max_length=MAX_ENCODER,
-            model=model
-        )
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-nl", use_fast=True)
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-nl")
+data = load_dataset("AhmedSSoliman/CoNaLa-Large")
+trainset = data["train"].map(map_to_encoder_decoder_inputs, batched=True, batch_size=1, remove_columns=['intent', 'snippet'])
+validset = data["validation"].map(map_to_encoder_decoder_inputs, batched=True, batch_size=1, remove_columns=['intent', 'snippet'])
+trainset.set_format(type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"])
+validset.set_format(type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"])
+data_collator = DataCollatorForSeq2Seq(
+    tokenizer=tokenizer,
+    max_length=MAX_ENCODER,
+    model=model
+)
+training_args = Seq2SeqTrainingArguments(
+    output_dir="./models/v1.0.0_exp01",
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    per_device_train_batch_size=BATCH_SIZE,
+    per_device_eval_batch_size=1,
+    num_train_epochs=EPOCH,
+    do_train=True,
+    do_eval=True,
+    fp16=False,
+    overwrite_output_dir=True,
+    learning_rate=LR,
+    weight_decay=0.01,
+    warmup_ratio=0.05,
+    seed=1995,
+    load_best_model_at_end=True
+)
+trainer = Seq2SeqTrainer(
+    model=model,
+    tokenizer=tokenizer,
+    args=training_args,
+    compute_metrics=CodeGenerationEvaluator,
+    data_collator=data_collator,
+    train_dataset=trainset,
+    eval_dataset=validset,
+)
 
-        training_args = Seq2SeqTrainingArguments(
-            output_dir="./models/v1.0.0_exp01",
-            evaluation_strategy="epoch",
-            save_strategy="epoch",
-            per_device_train_batch_size=BATCH_SIZE,
-            per_device_eval_batch_size=1,
-            num_train_epochs=EPOCH,
-            do_train=True,
-            do_eval=True,
-            fp16=False,
-            overwrite_output_dir=True,
-            learning_rate=LR,
-            weight_decay=0.01,
-            warmup_ratio=0.05,
-            seed=1995,
-            load_best_model_at_end=True
-        )
-        self.trainer = Seq2SeqTrainer(
-            model=model,
-            tokenizer=tokenizer,
-            args=training_args,
-            compute_metrics=CodeGenerationEvaluator,
-            data_collator=data_collator,
-            train_dataset=trainset,
-            eval_dataset=validset,
-        )
-
-    def forward(self):
-        self.trainer.train()
-
-
-if __name__ == "__main__":
-    train = SingleGPU()
-    train()
+trainer.train()
